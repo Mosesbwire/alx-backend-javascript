fc34f8592901eed173f526e853a5db84831d540d
Mosesbwire <mosesdev92@gmail.com>
2023-10-02 17:20:37 +0300

    Add a finally block

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -2,13 +2,11 @@
 export default function handleResponseFromAPI(promise){
     const msg = "Got a response from the API"
     promise.then(()=> {
-        console.log(msg);
+        
         return {
             status: 200,
             body: 'Success'
         }
-    }, ()=>{
-        console.log(msg);
-        return new Error();
-    })
+    }).catch(err => new Error()).
+    finally(console.log(msg))
 }
