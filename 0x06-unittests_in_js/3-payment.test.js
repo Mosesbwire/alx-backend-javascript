3951cc4194dd9a8cb7a05ee61bf0fbac3cbbcade
Mosesbwire <mosesdev92@gmail.com>
2023-11-23 01:02:43 +0300

    split test into individual specific tests

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,9 +1,11 @@
+/* eslint-disable jest/valid-expect */
 /* eslint-disable jest/expect-expect */
 /* eslint-disable jest/prefer-expect-assertions */
 /* eslint-disable func-names */
 /* eslint-disable prefer-arrow-callback */
 /* eslint-disable import/no-extraneous-dependencies */
 
+const { expect } = require('chai');
 const sinon = require('sinon');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
@@ -11,10 +13,28 @@ const Utils = require('./utils');
 // eslint-disable-next-line prefer-destructuring
 
 describe('sendPaymentRequestToApi', function () {
-  it('should call calculateNumber once with given arguments', function () {
-    const calculateNumber = sinon.spy(Utils, 'calculateNumber');
+  let calculateSpy;
+  this.beforeEach(function () {
+    calculateSpy = sinon.spy(Utils, 'calculateNumber');
+  });
+  this.afterEach(function () {
+    calculateSpy.restore();
+  });
+  it('should call calculateNumber once', function () {
     sendPaymentRequestToApi('SUM', 100, 20);
-    calculateNumber.restore();
-    calculateNumber.calledOnceWith('SUM', 100, 20);
+    // eslint-disable-next-line no-unused-expressions
+    expect(calculateSpy.calledOnce).to.be.true;
+  });
+  it('should call calculateNumber with correct arg Sum', function () {
+    sendPaymentRequestToApi('SUM', 100, 20);
+    calculateSpy.calledWith('SUM');
+  });
+  it('should call calculateNumber with correct arg 100', function () {
+    sendPaymentRequestToApi('SUM', 100, 20);
+    calculateSpy.calledWith(100);
+  });
+  it('should call calculateNumber with correct arg 20', function () {
+    sendPaymentRequestToApi('SUM', 100, 20);
+    calculateSpy.calledWith(20);
   });
 });
