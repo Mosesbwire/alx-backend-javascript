059f2e53cff888c6c58dc9131f0bf6f68fc6156d
Mosesbwire <mosesdev92@gmail.com>
2023-11-23 12:18:07 +0300

    Intergration test from task 9. A simple express server

--- 0x06-unittests_in_js/9-api/api.js
+++ 0x06-unittests_in_js/9-api/api.js
@@ -5,6 +5,15 @@ const app = express();
 app.get('/', (req, res) => {
   res.status(200).send('Welcome to the payment system');
 });
+
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params;
+  if (!Number(id)) {
+    return res.status(404).send('Not found');
+  }
+  return res.status(200).send(`Payment methods for cart: ${id}`);
+});
+
 app.listen(7865, () => {
   console.log('API available on localhost port 7865');
 });
---
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -0,0 +1,38 @@
+/* eslint-disable jest/valid-expect */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable jest/prefer-expect-assertions */
+const { expect } = require('chai');
+const request = require('request');
+
+describe('test index page', () => {
+  it('should return statusCode 200 on success', function () {
+    request('http://localhost:7865/cart/200', (error, response, body) => {
+      const statusCode = 200;
+      expect(statusCode).to.be.equal(response.statusCode);
+    });
+  });
+  it('should return string as response on success', function () {
+    request('http://localhost:7865/cart/200', (error, response, body) => {
+      const respBody = 'Payment methods for cart: 200';
+      expect(respBody).to.be.equal(body);
+    });
+  });
+  it('should return OK as response message on success', function () {
+    request('http://localhost:7865/cart/200', (error, response, body) => {
+      expect('OK').to.be.equal(response.statusMessage);
+    });
+  });
+  it('should return statusCode 404 on incorrect id type', function () {
+    request('http://localhost:7865/cart/abc', (error, response, body) => {
+      const statusCode = 404;
+      expect(statusCode).to.be.equal(response.statusCode);
+    });
+  });
+  it('should return correct 404 message on incorrect id type', function () {
+    request('http://localhost:7865/cart/abc', (error, response, body) => {
+      const respBody = 'Not found';
+      expect(respBody).to.be.equal(body);
+    });
+  });
+});
