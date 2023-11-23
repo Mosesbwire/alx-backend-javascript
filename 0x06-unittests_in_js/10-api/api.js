7b263f05c7bac0006360b6c4f6c85de683f90184
Mosesbwire <mosesdev92@gmail.com>
2023-11-23 13:39:53 +0300

    more intergration tests for task 10

---
+++ 0x06-unittests_in_js/10-api/api.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get('/', (req, res) => {
+  res.status(200).send('Welcome to the payment system');
+});
+
+app.get('/cart/:id', (req, res) => {
+  const { id } = req.params;
+  if (!Number(id)) {
+    return res.status(404).send('Not found');
+  }
+  return res.status(200).send(`Payment methods for cart ${id}`);
+});
+
+app.get('/available-payments', (req, res) => {
+  const data = {
+    payment_methods: {
+      credit_cards: true,
+      paypal: false,
+    },
+  };
+  res.status(200).send(data);
+});
+
+app.post('/login', (req, res) => {
+  const { userName } = req.body;
+  res.status(200).send(`Welcome ${userName}`);
+});
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
---
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -0,0 +1,79 @@
+/* eslint-disable jest/valid-expect */
+/* eslint-disable func-names */
+/* eslint-disable prefer-arrow-callback */
+/* eslint-disable jest/prefer-expect-assertions */
+const { expect, assert } = require('chai');
+const request = require('request');
+
+describe('test index page', () => {
+  it('should return statusCode 200 on success', function () {
+    request('http://localhost:7865/cart/200', (error, response, body) => {
+      const statusCode = 200;
+      expect(statusCode).to.be.equal(response.statusCode);
+    });
+  });
+  it('should return string as response on success', function () {
+    request('http://localhost:7865/cart/200', (error, response, body) => {
+      const respBody = 'Payment methods for cart 200';
+      expect(respBody).to.be.equal(body);
+    });
+  });
+  it('should return OK as response message on success', function () {
+    request('http://localhost:7865/cart/200', (error, response, body) => {
+      expect('OK').to.be.equal(response.statusMessage);
+    });
+  });
+  it('should return statusCode 404 on incorrect id type', function () {
+    request('http://localhost:7865/cart/abc', (error, response, body) => {
+      const statusCode = 404;
+      expect(statusCode).to.be.equal(response.statusCode);
+    });
+  });
+  it('should return correct 404 message on incorrect id type', function () {
+    request('http://localhost:7865/cart/abc', (error, response, body) => {
+      const respBody = 'Not found';
+      expect(respBody).to.be.equal(body);
+    });
+  });
+});
+
+describe('available-payments', function () {
+  it('should return statusCode 200 on success', function () {
+    request('http://localhost:7865/available-payments', (error, response, body) => {
+      const statusCode = 200;
+      expect(statusCode).to.be.equal(response.statusCode);
+    });
+  });
+  // eslint-disable-next-line jest/expect-expect
+  it('should return object on success', function () {
+    request('http://localhost:7865/available-payments', (error, response, body) => {
+      const objResp = {
+        payment_methods: {
+          credit_cards: true,
+          paypal: false,
+        },
+      };
+      assert.deepEqual(objResp, JSON.parse(body));
+    });
+  });
+});
+
+describe('login', function () {
+  it('should return statusCode 200 on success', function () {
+    request({
+      method: 'POST',
+      uri: 'http://localhost:7865/login',
+      body: JSON.stringify({ userName: 'Moses' }),
+    }, (err, resp, body) => {
+      const statusCode = 200;
+      expect(statusCode).to.be.equal(resp.statusCode);
+    });
+  });
+  it('should return correct string response on success', function () {
+    const userName = 'User';
+    request.post({ url: 'http://localhost:7865/login', form: { userName } }, (err, resp, body) => {
+      const respBody = `Welcome ${userName}`;
+      expect(respBody).to.be.equal(body);
+    });
+  });
+});
